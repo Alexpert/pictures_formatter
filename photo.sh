9308bb71500a24a29637a179ab22a3b2793d8397
Rob <rob@okcomputerv.home>
2020-12-24 16:09:35 +0100

    Hurray

--- photo.sh
+++ photo.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-folders=($(find -type d -name "export"))
+folders=($(find -type d -name "Export"))
 
 
 folder_handler () {
+  numFiles=$(ls $1/*.jpg | wc -l)
+  indexFile=0
   for picture in $1/*.jpg
   do
+    echo -e "\tShrinking photo $indexFile of $numFiles: $picture"
+    ((indexFile+=1))
     identity=$(identify $picture 2> /dev/null)
     if [ "$identity" != '' ]
-      then
-        resolution=$(echo $identity | cut -d' ' -f3)
-        echo $picture $resolution
-        if [ $(echo $resolution | cut -d'x' -f1) \> $(echo $resolution | cut -d'x' -f2) ]
-          then
-            echo "paysage"
-          else
-            echo "portrait"
-          fi
-        picture=${picture/.\//}
-        picture=${picture/\/export\//_}
-        picture=${picture/\//_}
-      fi
+    then
+        exportFile=${picture/.\//}
+        exportFile=${exportFile/\/Export\//_}
+        exportFile=${exportFile/\//_}
+        
+        convert $picture -resize 1920x1920 resize/$exportFile
+    fi
   done
 }
 
+mkdir resize
+numDir=${#folders[@]}
+indexDir=0
 for curr_folder in "${folders[@]}"
 do
+  ((indexDir+=1))
+  echo -e "\nDir $indexDir of $numDir : $curr_folder"
   folder_handler $curr_folder
 done
