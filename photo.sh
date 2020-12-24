ba116d3478f39350ac05596e617bbb5f84f51046
Alexandre SALMON <salmon.alexandre10@gmail.com>
2020-12-24 15:36:39 +0100

    image validation

--- photo.sh
+++ photo.sh
@@ -5,10 +5,15 @@ folders=($(find -type d -name "export"))
 folder_handler () {
   for picture in $1/*.jpg
   do
+    identity=$(identify $picture 2> /dev/null)
+    if [ "$identity" != '' ]
+      then
+        
+        echo $picture' is valid'
+      fi
     picture=${picture/.\//}
     picture=${picture/\/export\//_}
     picture=${picture/\//_}
-    echo $picture
 
   done
 }
